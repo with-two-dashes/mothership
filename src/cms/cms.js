b4f383d000d2a961a0b99e170b04c05acec76860
Victor Lawrence <victor.lawrence@uga.edu>
2019-09-26 16:23:14 -0400

    updated CMS config

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,18 +2,16 @@ import { init } from 'netlify-cms-app'
 import collections from './collections'
 
 const mediaFolder = `${__dirname}/src/images`
-const publicFolder = `${__dirname}/src/pages`
 
 init({
   config: {
-    display_url: 'localhost:8080',
+    display_url: 'https://www.with-two-dashes.com',
     backend: {
       name: 'git-gateway'
     },
     load_config_file: false,
     media_folder_relative: true,
     collections,
-    media_folder: mediaFolder,
-    public_folder: publicFolder
+    media_folder: mediaFolder
   }
 })
